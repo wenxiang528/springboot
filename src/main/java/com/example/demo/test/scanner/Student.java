fa5543d8b01f4b335b8009c16c5a12ce7a7a0e75
wenxiang <1002404000@qq.com>
2022-12-09 15:21:11 +0800

    提交

---
+++ src/main/java/com/example/demo/test/scanner/Student.java
@@ -0,0 +1,13 @@
+package com.example.demo.test.scanner;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Student {
+    private  String age;
+
+    private BigDecimal money;
+
+}
